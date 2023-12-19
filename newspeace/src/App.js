c4645851979ac66164c1880b4fd6b87beeaac2b2
yujin <jyj7452@naver.com>
2023-12-19 11:14:10 +0900

    마이페이지 - 수정 페이지

--- newspeace/src/App.js
+++ newspeace/src/App.js
@@ -12,6 +12,7 @@ import Mypage from './components/mypage.js';
 import Result from './components/result.js';
 import Notice from './components/notice.js';
 import Logout from './components/logout.js';
+import Editpage from './components/editPage.js';
 import styled from "styled-components"; //npm i styled-components
 
 const AllWrapper = styled.div`
@@ -38,6 +39,7 @@ function App() {
               <Route exact path="/mypage" element={<Mypage/>} />
               <Route exact path="/notice" element={<Notice/>} />
               <Route exact path="/logout" element={<Logout/>} />
+              <Route exact path="/editPage" element={<Editpage/>} />
           </Routes>
         </Router>
           
