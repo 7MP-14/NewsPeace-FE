9c386842d665e60a8c2bdadd72ff5a3670b5dff5
yujin <jyj7452@naver.com>
2023-12-28 15:05:34 +0900

    키워드차트-주식 백 연동

--- newspeace/src/components/myChart.js
+++ newspeace/src/components/myChart.js
@@ -8,29 +8,70 @@ const LineChart = () => {
   const location = useLocation();
   const keywordText = location.state?.keywordText || '키워드'; // state가 정의되지 않은 경우 기본값 사용
   
-  const [series, setSeries] = useState([]);
+  const [sentimentSeries, setSentimentSeries] = useState([]);
+  const [stockSeries, setStockSeries] = useState([]);
   const [options, setOptions] = useState({
-    chart: {
-      height: 350,
-      type: 'line',
-      zoom: {
+    sentimentOptions: {
+      chart: {
+        height: 350,
+        type: 'line',
+        zoom: {
+          enabled: false
+        },
+        background: '#ffffff'
+      },
+      dataLabels: {
         enabled: false
       },
-      background: '#ffffff'
+      stroke: {
+        curve: 'straight'
+      },
+      
+      xaxis: {
+        type: 'category',
+      },
+      yaxis: {
+        max: 120,
+        min: 0
+      }
     },
-    dataLabels: {
-      enabled: false
-    },
-    stroke: {
-      curve: 'straight'
-    },
-    
-    xaxis: {
-      type: 'category',
-    },
-    yaxis: {
-      max: 120,
-      min: 0
+    stockOptions: {
+      chart: {
+        height: 350,
+        type: 'area',
+        zoom: {
+          autoScaleYaxis: true
+        },
+        background: '#ffffff'
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth'
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          inverseColors: false,
+          opacityFrom: 0.5,
+          opacityTo: 0,
+          stops: [0, 90, 100]
+        }
+      },
+      xaxis: {
+        type: 'datetime',
+      },
+      yaxis: {
+        max: 100000,
+        min: 0
+      },
+      tooltip: {
+        x: {
+          format: 'dd MMM yyyy'
+        }
+      }
     }
   });
 
@@ -47,7 +88,7 @@ const LineChart = () => {
           
         });
         const data = await response.json();
-
+        console.log(data);
         // 데이터 처리
         const times = data.result_time.map(time => {
           const [date, timeOfDay] = time.split('T');
@@ -56,27 +97,40 @@ const LineChart = () => {
           return `${month}/${day} ${hours}:${minutes}`; // MM/DD HH:MM 형태로 반환
         });
         const negatives = data.result_negative.map(value => (value === -1 ? 0 : value));
-
-        // 차트 데이터 설정
-        setSeries([{
-          name: '부정도 %',
-          data: negatives
-        }]);
+        
+        // 주식 데이터
+        const stockData = data.result_present.map(present => {
+            return {
+              x: new Date(present.date).getTime(), 
+              y: present.value
+            };
+          });
+        // console.log(stockData);
+        setSentimentSeries([{ name: '부정도 %', data: negatives }]);
+        setStockSeries([{ name: '주식 가격', data: stockData }]);
 
         // 차트 옵션 설정
         setOptions(prevOptions => ({
-          ...prevOptions,
-          xaxis: {
-            ...prevOptions.xaxis,
-            categories: times
-          }
-        }));
-
-
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+            ...prevOptions,
+            sentimentOptions: {
+              ...prevOptions.sentimentOptions,
+              xaxis: {
+                ...prevOptions.sentimentOptions.xaxis,
+                categories: times
+              }
+            },
+            stockOptions: {
+              ...prevOptions.stockOptions,
+              xaxis: {
+                ...prevOptions.stockOptions.xaxis,
+                type: 'datetime'
+              }
+            }
+          }));
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
 
     fetchData();
   }, [keywordText]);
@@ -86,7 +140,11 @@ const LineChart = () => {
       <div className='chart-container'>
         <div className='chart-title'><strong>{keywordText}</strong> 시간별 부정률(%)</div>
         <div className='chart-box'>
-          <ReactApexChart options={options} series={series} type='line' height={350} />
+          <ReactApexChart options={options.sentimentOptions} series={sentimentSeries} type='line' height={350} />
+        </div>
+        <div className='chart-title'>주식 가격 추이</div>
+        <div className='chart-box'>
+          <ReactApexChart options={options.stockOptions} series={stockSeries} type='area' height={350} />
         </div>
       </div>
     </div>
