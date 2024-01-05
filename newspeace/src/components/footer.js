331393e690d0fc8299bf782e06031aa177fb2c2d
Dh-Off <heonbella@naver.com>
2024-01-05 14:01:15 +0900

    footer, home 배치 변경

--- newspeace/src/components/footer.js
+++ newspeace/src/components/footer.js
@@ -2,41 +2,44 @@ import styled from "styled-components";
 import React from "react";
 import '../css/header.css';
 
+const StyledFooter = styled.footer`
+  background-color: #F0F0F0;
+  border-top-left-radius: 5rem; 
+  border-top-right-radius: 5rem; 
+  `;
 
-export default function Header(props) {
+export default function Footer(props) {
   return (
-    <footer class="footer bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
-                <ul class="list-inline mb-2">
-                    <li class="list-inline-item"><a href="#!">About</a></li>
-                    <li class="list-inline-item">⋅</li>
-                    <li class="list-inline-item"><a href="#!">Contact</a></li>
-                    <li class="list-inline-item">⋅</li>
-                    <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
-                    <li class="list-inline-item">⋅</li>
-                    <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
-                </ul>
-                <p class="text-muted small mb-4 mb-lg-0">Copyright&copy; 2023-2024 by NewsPeace. All Rights Reserved.</p>
-            </div>
-            <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
-                <ul class="list-inline mb-0">
-                    <li class="list-inline-item me-4">
-                        <a href="#!"><i class="bi-facebook fs-3"></i></a>
-                    </li>
-                    <li class="list-inline-item me-4">
-                        <a href="#!"><i class="bi-twitter fs-3"></i></a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#!"><i class="bi-instagram fs-3"></i></a>
-                    </li>
-                </ul>
-            </div>
+    <StyledFooter className="footer" style={{ backgroundColor: '#F0F0F0' }}>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 h-100 text-center text-lg-start my-auto">
+            <ul className="list-inline mb-2">
+              <li className="list-inline-item"><a href="#!">About</a></li>
+              <li className="list-inline-item">⋅</li>
+              <li className="list-inline-item"><a href="#!">Contact</a></li>
+              <li className="list-inline-item">⋅</li>
+              <li className="list-inline-item"><a href="#!">Terms of Use</a></li>
+              <li className="list-inline-item">⋅</li>
+              <li className="list-inline-item"><a href="#!">Privacy Policy</a></li>
+            </ul>
+            <p className="text-muted small mb-4 mb-lg-0">Copyright&copy; 2023-2024 by NewsPeace. All Rights Reserved.</p>
+          </div>
+          <div className="col-lg-6 h-100 text-center text-lg-end my-auto">
+            <ul className="list-inline mb-0">
+              <li className="list-inline-item me-4">
+                <a href="#!"><i className="bi-facebook fs-3"></i></a>
+              </li>
+              <li className="list-inline-item me-4">
+                <a href="#!"><i className="bi-twitter fs-3"></i></a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#!"><i className="bi-instagram fs-3"></i></a>
+              </li>
+            </ul>
+          </div>
         </div>
-    </div>
-    </footer>
-    
+      </div>
+    </StyledFooter>
   );
 }
-
--- newspeace/src/components/home.js
+++ newspeace/src/components/home.js
@@ -1,16 +1,21 @@
 import {React, useEffect, useState} from 'react';
+//import { useSpring, animated } from 'react-spring';
+
 // import '../css/home.css';
-import styled from "styled-components";
+// import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
+import first from '../img/first.png';
+import second from '../img/second.png';
+import chatbot from '../img/chatbot.png';
+import qr from '../img/qr.png';
 import icon1 from '../img/흰돋보기.png';
-import icon2 from '../img/의사결정.png';
-import icon3 from '../img/효율성.png';
-import icon4 from '../img/전략2.png';
-import icon5 from '../img/정장.jpg';
-import icon6 from '../img/의사소통.jpg';
-import icon7 from '../img/업무.jpg';
-import Loading from './Loading.js';
-import anno from '../img/user.png'
+// import icon2 from '../img/의사결정.png';
+// import icon3 from '../img/효율성.png';
+// import icon4 from '../img/전략2.png';
+//import icon5 from '../img/정장.jpg';
+//import icon6 from '../img/의사소통.jpg';
+//import icon7 from '../img/업무.jpg';
+import Service from '../img/Service.png';
 import people1 from '../img/people1.png';
 import people2 from '../img/people2.jpg';
 import people3 from '../img/people3.jpg';
@@ -20,7 +25,25 @@ import people6 from '../img/people6.png';
 import people7 from '../img/people7.jpg';
 import people8 from '../img/people8.png';
 
+import Loading from './Loading.js';
+
 const Home=()=>{
+
+  // 투명도
+  /////
+  
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 }); // 마우스 위치를 저장할 state 추가
+
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove); // 마우스 이동 이벤트 추가
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove); // 컴포넌트가 언마운트될 때 이벤트 제거
+    };
+  }, []);
 
   const [checkedItems, setCheckedItems] = useState([]);
   const [inputkeyword, setinputKeyword] = useState('');
@@ -61,7 +84,7 @@ const Home=()=>{
   
     setLoading(true);
   
-    fetch('/news/search/', {
+    fetch('http://newspeace.co.kr/news/search/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
@@ -92,7 +115,7 @@ const Home=()=>{
 
 
   useEffect(() => {
-    fetch('/hot/', {
+    fetch('http://newspeace.co.kr/hot/', {
       method: 'GET',
     })
       .then((res) => res.json())
@@ -110,7 +133,6 @@ const Home=()=>{
     const intervalId = setInterval(() => {
       // 먼저 애니메이션 클래스를 제거
       setAnimationClass('');
-
       // 약간의 지연 후에 애니메이션 클래스를 다시 적용
       setTimeout(() => {
         setAnimationClass('keyword-animation-enter');
@@ -124,7 +146,7 @@ const Home=()=>{
   const hotkeywordsubmit=()=>{
     setLoading(true);
 
-    fetch('/news/search/', {
+    fetch('http://newspeace.co.kr/news/search/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json; charset=utf-8'
@@ -145,9 +167,6 @@ const Home=()=>{
         console.error('에러:', error);
       });
   }
-
-
-
     return (  
       <>
 
@@ -158,123 +177,136 @@ const Home=()=>{
        :
        (
         <>
-        <div>
-          <header className="masthead">
-          <div className="container position-relative">
-            <div className="row justify-content-center">
-              <div className="col-xl-6">
-                <div className="text-center text-white">
-                  <div className="form-subscribe" id="contactForm">
-                    <div className="row">
-                      <div className="col">
-                        <input
-                          className="form-control form-control-lg"
-                          id="keyword"
-                          type="text"
-                          value={inputkeyword}
-                          onChange={handleKeywordChange}
-                          onKeyDown={handleKeyPress}
-                          placeholder="관심 있는 키워드를 입력하세요."
-                        />
-                        <div className="categorybtn">
-                            <button className={`checkbtn ${checkedItems.includes('정치') ? 'selected' : ''}`} onClick={() => checkedItemHandler('정치')}>정치</button>
-                            <button className={`checkbtn ${checkedItems.includes('경제') ? 'selected' : ''}`} onClick={() => checkedItemHandler('경제')}>경제</button>
-                            <button className={`checkbtn ${checkedItems.includes('사회') ? 'selected' : ''}`} onClick={() => checkedItemHandler('사회')}>사회</button>
-                            <button className={`checkbtn ${checkedItems.includes('문화') ? 'selected' : ''}`} onClick={() => checkedItemHandler('문화')}>문화</button>
-                            {/* <button className={`checkbtn ${checkedItems.includes('국제') ? 'selected' : ''}`} onClick={() => checkedItemHandler('국제')}>국제</button> */}
-                            <button className={`checkbtn ${checkedItems.includes('IT') ? 'selected' : ''}`} onClick={() => checkedItemHandler('IT')}>IT</button>
-                            <button className={`checkbtn ${checkedItems.includes('연예') ? 'selected' : ''}`} onClick={() => checkedItemHandler('연예')}>연예</button>
-                            <button className={`checkbtn ${checkedItems.includes('스포츠') ? 'selected' : ''}`} onClick={() => checkedItemHandler('스포츠')}>스포츠</button>
+        <div
+          className="blurred-background"
+          style={{
+            width: '160px',
+            height: '160px',  
+            borderRadius: '50%',
+            background: '#2D78EF',    // #1774D0
+            position: 'fixed',
+            top: mousePosition.y - 40,
+            left: mousePosition.x - 60,
+            zIndex: -1,
+            pointerEvents: 'none',
+          }}
+        ></div>
+        
+        <div className="ani-size"> 
+          <header className="masthead" >
+            <div className="container position-relative">
+              <div className="row justify-content-center">
+                <div className="col-xl-6">
+                  <div className="text-center text-white">
+                    <div className="form-subscribe" id="contactForm">
+                      <div className="row">
+                        <div className="col">
+                          <input
+                            className="form-control form-control-lg"
+                            id="keyword"
+                            type="text"
+                            value={inputkeyword}
+                            onChange={handleKeywordChange}
+                            onKeyDown={handleKeyPress}
+                            placeholder="관심 있는 키워드를 입력하세요."
+                          />
+                          <div className="categorybtn">
+                              <button className={`checkbtn ${checkedItems.includes('정치') ? 'selected' : ''}`} onClick={() => checkedItemHandler('정치')}>정치</button>
+                              <button className={`checkbtn ${checkedItems.includes('경제') ? 'selected' : ''}`} onClick={() => checkedItemHandler('경제')}>경제</button>
+                              <button className={`checkbtn ${checkedItems.includes('사회') ? 'selected' : ''}`} onClick={() => checkedItemHandler('사회')}>사회</button>
+                              <button className={`checkbtn ${checkedItems.includes('문화') ? 'selected' : ''}`} onClick={() => checkedItemHandler('문화')}>문화</button>
+                              {/* <button className={`checkbtn ${checkedItems.includes('국제') ? 'selected' : ''}`} onClick={() => checkedItemHandler('국제')}>국제</button> */}
+                              <button className={`checkbtn ${checkedItems.includes('IT') ? 'selected' : ''}`} onClick={() => checkedItemHandler('IT')}>IT</button>
+                              <button className={`checkbtn ${checkedItems.includes('연예') ? 'selected' : ''}`} onClick={() => checkedItemHandler('연예')}>연예</button>
+                              <button className={`checkbtn ${checkedItems.includes('스포츠') ? 'selected' : ''}`} onClick={() => checkedItemHandler('스포츠')}>스포츠</button>
 
-                        </div>
-                        <div className="hottopic">
-                          <div className="label-container">
-                            <p className="label">인기 검색어:</p>
                           </div>
-                         <div className="keyword-container">
-                          {hotKeywords.length > 0 &&
-                            <p className={`keyword ${animationClass}`} style={{cursor: 'pointer'}} onClick={() => hotkeywordsubmit(hotKeywords[currentHotKeywordIndex])} >{currentHotKeywordIndex + 1}. {hotKeywords[currentHotKeywordIndex]}</p>
-                          }
+                          
+                          <div className="hottopic">
+                            <div className="label-container">
+                              <p className="label">인기 검색어:</p>
+                            </div>
+                          <div className="keyword-container">
+                            {hotKeywords.length > 0 &&
+                              <p className={`keyword ${animationClass}`} style={{cursor: 'pointer'}} onClick={() => hotkeywordsubmit(hotKeywords[currentHotKeywordIndex])} >{currentHotKeywordIndex + 1}. {hotKeywords[currentHotKeywordIndex]}</p>
+                            }
+                          </div>
+                          </div>
                         </div>
+                        <div className="col-auto">
+                          <img src={icon1} style={{ width: '40px', height: '37px', cursor: 'pointer'}} onClick={submit} alt="search icon" />
                         </div>
-                      </div>
-                      <div className="col-auto">
-                        <img src={icon1} style={{ width: '40px', height: '37px', cursor: 'pointer'}} onClick={submit} alt="search icon" />
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+          </header>
+
+          <hr></hr>
+          <div className="showcase"  style={{paddingTop:'2rem'}}>
+            <div className="container-fluid p-0" style={{ width: '90%', margin: '0 auto', display: 'flex' }}>
+              <div className="col-lg-6" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <img src={Service} style={{ width: '70%', height: 'auto' }} alt="Service" />
+              </div>
+
+              <div className="col-lg-6">
+                <div className="showcase-text" style={{ width: '100%', height: 'auto' }}>
+                  <h2><b>NewsPeace</b>는 어떤 서비스인가요 ? </h2>  
+                  <div className="feature" style={{ marginTop: '5rem' }}>
+                    <h3>일주일 단위 기사 데이터 수집</h3>
+                    <p style={{ fontSize: '1.07rem' }}>최근 일주일 동안의 뉴스기사를 수집하여 검색한 키워드에 대한 기사를 분석합니다.</p>
+                  </div>
+
+                  <div className="feature" style={{ marginTop: '3rem' }}>
+                    <h3>뉴스기사 별 긍정, 부정률 분석</h3>
+                    <p style={{ fontSize: '1.07rem' }}>AI 모델을 활용하여 각 기사의 감정분석을 수행하고 긍정과 부정의 비율을 제공합니다.</p>
+                  </div>
+
+                  <div className="feature" style={{ marginTop: '3rem' }}>
+                    <h3>실시간 주가 정보 확인</h3>
+                    <p style={{ fontSize: '1.07rem' }}>증권정보를 실시간으로 반영해서 분석 결과에 따른 주가 파악에 도움이 될 수 있습니다.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </header>
 
-          <section className="features-icons bg-light text-center">
-              <div className="container">
-                  <div className="row">
-                      <div className="col-lg-4">
-                          <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+          
+          <div className="chatbot text-center" style={{width:'70%', margin: '0 auto'}}>
+            <div className="image-and-heading">
+              <hr></hr>
+              <h2><img src={chatbot} style={{ width: '4%', height: '10%', paddingTop:'2rem'}} alt="chatbot" />
+              챗봇 서비스
+              </h2>
+            </div>
+            <p style={{ fontSize: '1.2rem' }}>카카오톡 ➡️ <b>"뉴스피스(Newspeace)"</b> 채널 등록 후 서비스를 이용해보세요!</p>
+            <p style={{ fontSize: '1.0rem' }}>🐶사진을 확대하려면 마우스를 올려주세요.🐶</p>
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop:'3rem', marginBottom:'4rem'}}>
+              <img
+                src={second}
+                style={{ width: '15%', height: '20%', margin: '0 4rem', transition: 'transform 0.3s' }}
+                alt="second"
+                className="enlarge-on-hover"
+              />
+              <img
+                src={first}
+                style={{ width: '15%', height: '20%', margin: '0 4rem', transition: 'transform 0.3s' }}
+                alt="first"
+                className="enlarge-on-hover"
+              />
+            </div>
+            <img src={qr} style={{ width: '5%', height: '5%'}} alt="qr" className="qr-hover"/>
+            <p style={{ fontSize: '0.8rem', marginBottom:'3rem'}}>[QR코드]</p>
+            
+          </div>
 
-                              <div className="col-auto"><img src={icon2} style={{width:'150px', height:'150px'}}></img></div>
-                              <h3>의사결정 지원</h3>
-                              <p className="lead mb-0">투자 의사결정 지원, 시장예측, 리스크 관리, 고객 서비스 향상</p>
-                          </div>
-                      </div>
-                      <div className="col-lg-4">
-                          <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
 
-                              <div className="col-auto"><img src={icon3} style={{width:'150px', height:'150px'}}></img></div>
-                              <h3>업무 효율성 향상</h3>
-                              <p className="lead mb-0">이슈, 트렌드, 시장 조사와 분석 시간을 줄임으로써 업무 효율성 증가</p>
-                          </div>
-                      </div>
-                      <div className="col-lg-4">
-                          <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-
-                              <div className="col-auto"><img src={icon4} style={{width:'150px', height:'150px'}}></img></div>
-                              <h3>전략적 대응</h3>
-                              <p className="lead mb-0">기업, 정부 차원에의 부정인 뉴스에 대한 선제적인 대응 가능</p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-
-          <section className="showcase">
-              <div className="container-fluid p-0">
-                  <div className="row g-0">
-                      {/* <div className="col-lg-6 order-lg-2 text-white showcase-img" style={"background-image: url('assets/img/bg-showcase-1.jpg')"}></div> */}
-                      <div className="col-lg-6 order-lg-2 text-white showcase-img" ><img src={icon7} style={{width:'100%', height:'100%'}} ></img></div>
-
-                      <div className="col-lg-6 order-lg-1 my-auto showcase-text">
-                          <h2>의사결정 지원</h2>
-                          <p className="lead mb-0">주식이나 기업 가치 분석, 특정 키워드 분석에 효과적으로 사용 가능, 기업에 대한 뉴스를 실시간으로 제공하기 때문에 사용자는 기업의 현재 상황을 빠르게 파악, 사용자는 기업의 성과와 함께, 기업의 사회적 책임과 윤리적 측면을 함께 고려 가능</p>
-                      </div>
-                  </div>
-                  <div className="row g-0">
-                      {/* <div className="col-lg-6 text-white showcase-img" style={"background-image: url('..//img/업무.jpg')"}></div> */}
-                      <div className="col-lg-6 text-white showcase-img"><img src={icon6} style={{width:'100%', height:'100%'}} ></img></div>
-
-                      {/* <div className="col-lg-6 text-white showcase-img"></div> */}
-
-                      <div className="col-lg-6 my-auto showcase-text">
-                          <h2>업무효율성 향상</h2>
-                          <p className="lead mb-0">협업이나 투자 시 특정 기업에 대한 분석, 자사 관련 기사의 동향 분석, 증권사와의 협업 및 투자사와의 협업 기대</p>
-                      </div>
-                  </div>
-                  <div className="row g-0">
-                      <div className="col-lg-6 order-lg-2 text-white showcase-img" ><img src={icon5} style={{width:'100%', height:'100%'}} ></img></div>
-                      <div className="col-lg-6 order-lg-1 my-auto showcase-text">
-                          <h2>전략적 대응</h2>
-                          <p className="lead mb-0">부정적인 뉴스를 신속히 감지하여 정부 기관과 기업 간 신뢰와 협력을 강화, 기업 뿐만 아니라 정부와 정책에 대한 부정적인 의견을 식별하여 선제적 대응 및 더 나은 해결 방안을 모색, 특정 인물의 현재 여론을 모니터링</p>
-                      </div>
-                  </div>
-              </div>
-          </section>
-
-          <section className="testimonials text-center bg-light">
+                            
+          <div className="testimonials text-center">
             <div className="container" ><hr></hr>
-                <h2 className="mb-5">KT Aivle 4기 ❤️14조❤️</h2>
+                <h2 className="mb-5" style={{marginTop:'4rem'}}>KT Aivle 4기 ❤️14조❤️</h2>
                 <div className="row">
                     <div className="col-lg-3">
                         <div className="testimonial-item mx-auto mb-5 mb-lg-0">
@@ -335,14 +367,13 @@ const Home=()=>{
                         </div>
                     </div>
                 </div>
+              </div>
             </div>
-        </section>
           </div>
         </>
-       )};
+       )}
       </>
-      );
-
+    );
   };
 
 export default Home;
