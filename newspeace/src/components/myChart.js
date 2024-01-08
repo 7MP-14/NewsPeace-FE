c2198ddf6f1430f9102ac871fd89d74aaeb822f5
yujin <jyj7452@naver.com>
2024-01-09 08:47:16 +0900

    마이차트-수정

--- newspeace/src/components/myChart.js
+++ newspeace/src/components/myChart.js
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom';
 import '../css/myChart.css';
 import backimg from "../img/bg-masthead.jpg";
 
+
 const LineChart = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
   const location = useLocation();
@@ -35,7 +36,7 @@ const LineChart = () => {
       },
       
       xaxis: {
-        type: 'datetime',
+        type: 'category',
       },
       yaxis: {
         max: 100,
@@ -43,7 +44,7 @@ const LineChart = () => {
       },
       tooltip: {
         x: {
-          format: 'dd MMM HH:mm' 
+          format: 'HH:mm' 
         },
         
       }
@@ -74,7 +75,8 @@ const LineChart = () => {
         }
       },
       xaxis: {
-        type: 'datetime',
+        type: 'category',
+        
       },
       yaxis: {
         max: undefined,
@@ -82,7 +84,7 @@ const LineChart = () => {
       },
       tooltip: {
         x: {
-          format: 'dd MMM HH:mm'
+          format: 'HH:mm'
         }
       },
       
@@ -106,55 +108,24 @@ const LineChart = () => {
         console.log(data);
 
         
-        // 부정도 데이터 처리
-        // const times = data.result_time.map(time => {
-        //   const [date, timeOfDay] = time.split('T');
-        //   const [year, month, day] = date.split('-');
-        //   const [hours, minutes] = timeOfDay.split(':');
-        //   return `${month}/${day} ${hours}:${minutes}`; // MM/DD HH:MM 형태로 반환
-        // });
-
-        const times = data.result_time.map(time => {
-          const [date, timeOfDay] = time.split('T');
-          const [hours, minutes] = timeOfDay.split(':');
-          return `${hours}:${minutes}`; // HH:MM 형태로 반환
+        // 시간 데이터를 HH:MM 형식으로 변환하여 차트 카테고리에 사용
+        const categories = data.result_time.map(time => {
+          const timePart = time.split('T')[1];
+          const [hours,] = timePart.split(':');
+          return `${hours}`; // 변환된 시간 문자열
         });
-
+        
+        // 부정도 차트 데이터 설정
         const negatives = data.result_negative.map(value => (value === -1 ? 0 : value));
-
-
-      // // UTC 기준으로 현재 날짜의 장 시작 시간과 마감 시간을 계산하는 함수
-      // const calculateMarketTimesUTC = () => {
-      //   // 현재 날짜를 기준으로 하는 Date 객체 생성
-      //   const now = new Date();
-      //   const marketOpen = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0)); // 오전 9시 KST는 UTC의 0시
-      //   const marketClose = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 6, 30, 0)); // 오후 3시 30분 KST는 UTC의 6시 30분
-
-      //   return {
-      //     marketOpenTime: marketOpen.getTime(),
-      //     marketCloseTime: marketClose.getTime()
-      //   };
-      // };
-        
-        
-        
-      if (data.result_present) {
-        // `stockData` 배열을 생성할 때, 각 데이터 포인트의 실제 타임스탬프로 `x` 값을 설정합니다.
-        const stockData = data.result_time.map((time, index) => {
-          const timestamp = new Date(time).getTime(); // `time` 문자열을 파싱하여 타임스탬프를 얻습니다.
-          return {
-            x: timestamp,
-            y: data.result_present[index]
-          };
-        }).sort((a, b) => a.x - b.x);
-
-        // // 장 시작과 마감 시간 계산
-        // const { marketOpenTime, marketCloseTime } = calculateMarketTimesUTC();
-      
-
         setSentimentSeries([{ name: '부정도', data: negatives }]);
-      
-       
+
+
+        // 주식 차트 데이터 설정
+        const stockData = data.result_present.map((value, index) => ({
+          x: new Date(data.result_time[index]).getTime(), // 변환된 날짜-시간 문자열
+          y: data.result_present[index]
+        }));
+        setStockSeries([{ name: '주식 가격', data: stockData }]);
 
         // 현재가, 전일 종가, 변동 금액, 변동률 계산
         setCurrentPrice(data.result_present[data.result_present.length - 1]);
@@ -165,9 +136,6 @@ const LineChart = () => {
         setOpenPrice(data.result_open);
         setHighPrice(data.result_high);
         setLowPrice(data.result_low);
-       
-        
-        setStockSeries([{ name: '주식 가격', data: stockData }]);
 
         
         setOptions(prevOptions => ({
@@ -177,55 +145,57 @@ const LineChart = () => {
             ...prevOptions.sentimentOptions,
             xaxis: {
               ...prevOptions.sentimentOptions.xaxis,
-              categories: data.result_time // 변환된 시간 데이터 설정
-            }
+              type: 'category',
+              categories: categories,
+            },
+            tooltip: {
+              x: {
+                formatter: function (val, { series, seriesIndex, dataPointIndex, w }) {
+                  const originalTime = data.result_time[dataPointIndex];
+                  const [datePart, timePart] = originalTime.split('T');
+                  const [year, month, day] = datePart.split('-');
+                  const [hours, minutes] = timePart.split(':');
+                  return `${month}/${day} ${hours}:${minutes}`; 
+                }
+              }
+            },
           },
 
       
           stockOptions: {
             ...prevOptions.stockOptions,
             xaxis: {
-              type: 'datetime'
-            }
-          
-            // annotations: {
-            //   xaxis: [
-            //     {
-            //       x: marketOpenTime,
-            //       borderColor: '#00E396',
-            //       label: {
-            //         text: '장 시작',
-            //         style: {
-            //           color: '#fff',
-            //           background: '#00E396',
-            //         },
-            //       },
-            //     },
-            //     {
-            //       x: marketCloseTime,
-            //       borderColor: '#FF4560',
-            //       label: {
-            //         text: '장 마감',
-            //         style: {
-            //           color: '#fff',
-            //           background: '#FF4560',
-            //         },
-            //       },
-            //     },
-            //   ]
-            // }
-          
+              ...prevOptions.stockOptions.xaxis,
+              type: 'category',
+              categories: categories,
+              // labels: {
+              //   formatter: function (value) {
+              //     return new Date(value).getHours(); // 타임스탬프를 시간(HH) 형식으로 변환
+              //   }
+              // }
+            },
+            tooltip: {
+              x: {
+                formatter: function (val, { series, seriesIndex, dataPointIndex, w }) {
+                  const originalTime = data.result_time[dataPointIndex];
+                  const [datePart, timePart] = originalTime.split('T');
+                  const [year, month, day] = datePart.split('-');
+                  const [hours, minutes] = timePart.split(':');
+                  return `${month}/${day} ${hours}:${minutes}`; 
+                }
+              }
+            },
           }
         }));
       
-      }
+      //}
         } catch (error) {
           console.error('Error fetching data:', error);
         }
       };
 
     fetchData();
-  }, [keywordText]);
+  }, [apiUrl, keywordText]); // 의존성 배열에 apiUrl 추가
 
   return (
     <div className='chart-background'>
