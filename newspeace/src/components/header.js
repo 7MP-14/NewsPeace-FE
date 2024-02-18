87c2b3d80fc1a86f6fab4d5bc7a0bf3962455b1d
Dh-Off <heonbella@naver.com>
2024-02-18 18:17:23 +0900

    classname 변경

--- newspeace/src/components/header.js
+++ newspeace/src/components/header.js
@@ -3,27 +3,27 @@ import '../css/header.css';
 export default function Header(props) {
   return (
     // Navigation
-    <nav class="navbar navbar-light bg-light static-top">
-        <div class="container">
-            <a class="navbar-brand" href="/"> <strong>Newspeace</strong></a>
+    <nav className="navbar navbar-light bg-light static-top">
+        <div className="container">
+            <a className="navbar-brand" href="/"> <strong>Newspeace</strong></a>
             <span className="menu">
             {
             props.login
             ?
             (
               <span>
-              <a class="btn btn-primary" href="/notice">공지사항</a>
-              <a class="btn btn-primary" href="/scrap">즐겨찾기</a>
-              <a class="btn btn-primary" href="/mypage">마이페이지</a>
-              <a class="btn btn-primary" href="/logout">로그아웃</a>
+              <a className="btn btn-primary" href="/notice">공지사항</a>
+              <a className="btn btn-primary" href="/scrap">즐겨찾기</a>
+              <a className="btn btn-primary" href="/mypage">마이페이지</a>
+              <a className="btn btn-primary" href="/logout">로그아웃</a>
               </span>
             )
             :
             (
               <span>
-                <a class="btn btn-primary" href="/notice">공지사항</a>
-                <a class="btn btn-primary" href="/login">로그인</a>
-                <a class="btn btn-primary" href="/signup">회원가입</a>
+                <a className="btn btn-primary" href="/notice">공지사항</a>
+                <a className="btn btn-primary" href="/login">로그인</a>
+                <a className="btn btn-primary" href="/signup">회원가입</a>
               </span>
             )
             }
