4e1261bc8f7344e3b0cccdb706c804198a8c1d20
Dh-Off <heonbella@naver.com>
2024-04-11 15:31:32 +0900

    검색결과 수정_1차

--- newspeace/src/components/result.js
+++ newspeace/src/components/result.js
@@ -3,10 +3,8 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import '../css/result.css'; // CSS 파일 import
 import scrap from '../img/빈책갈피.png';
 import scrapcomp from'../img/찬책갈피.png';
-import news_1 from '../img/신문배경.png';
-import article from '../img/기사배경.png';
 import useFormItemStatus from 'antd/es/form/hooks/useFormItemStatus';
-import people6 from '../img/null.png';
+import null_img from '../img/kpilogo.png';
 
 function Dashboard() {
     const apiUrl = process.env.REACT_APP_API_URL;
@@ -19,6 +17,33 @@ function Dashboard() {
     const positiveWidth = `${responseData.긍정도}%`; // 긍정 비율
     const negativeWidth = `${responseData.부정도}%`; // 부정 비율
     const neutralWidth = `${responseData.중립도}%`; // 중립 비율
+
+    // 기사별 리스트
+
+    const [showPositive, setShowPositive] = useState(false);
+    const [showNegative, setShowNegative] = useState(false);
+    const [showNeturl, setShowNeturl] = useState(false);
+
+    const handleTogglePositive = () => {
+        setShowPositive(!showPositive);
+        setShowNegative(false);
+        setShowNeturl(false);
+      };
+      
+      const handleToggleNegative = () => {
+        setShowPositive(false);
+        setShowNegative(!showNegative);
+        setShowNeturl(false);
+      };
+      
+      const handleToggleNeturl = () => {
+        setShowPositive(false);
+        setShowNegative(false);
+        setShowNeturl(!showNeturl);
+      };
+    
+      ///////////////////
+
 
     const [scrapped, setScrapped] = useState({}); // 스크랩 상태 관리 (스크랩 완료 시 이미지 변경)
 
@@ -169,21 +194,11 @@ const handleKeywordClick = (clickedKeyword) => {
         setinputKeyword(event.target.value);
       };
     return (
-        <div className="result_container mx-auto p-4" style={{height:'100%',backgroundImage: `url(${news_1})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="result_container mx-auto p-4" style={{height:'100%', backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <div className="flex-1 text-center">     
                 {/* <h1 className='Test'> 키워드 : </h1> */}
-                <h2 className="text-2xl font-bold mb-4">{responseData.search_keyword}</h2>
-                <div className="search-related-keywords">
-                    <p>
-                    연관 검색어 : {responseData.related_keyword.map((related_keyword, index) => (
-                        <span key={index} className="related_keyword" onClick={() => handleKeywordClick(related_keyword)}>
-                        {related_keyword}{index < responseData.related_keyword.length - 1 && ' '}
-                        </span>
-                    ))}
-                    </p>
-                </div>
-                <hr></hr>
-                <div className="introduction-text">
+                
+                <div className="introduction-text" style={{marginTop:'1rem', marginBottom:'2.5rem'}}>
                 {categoryString ? (
                     <>
                         카테고리 <b>{categoryString}</b>에서의 <b>'{responseData.search_keyword}'</b> 분석 결과입니다.
@@ -191,93 +206,127 @@ const handleKeywordClick = (clickedKeyword) => {
                 )
                 : (
                     <>
-                        <b>'{responseData.search_keyword}'</b> 분석 결과입니다.
+                        <h4><b>'{responseData.search_keyword}'</b> 분석 결과입니다.</h4>
                     </>
                 )}
                 </div>
-                <h1 className='Test1'>
-                    <span>긍정 의견 (%)</span>
-                    <span>부정 의견 (%)</span></h1>
             </div>
             <div className='contents'>
                 <div className="mb-4">
-                    <div className="bar-container mb-2" style={{ width: '100%' }}>
-                        <div className="positive-bar" style={{ width: positiveWidth }}>{positiveWidth}</div>
-                        <div className="negative-bar" style={{ width: negativeWidth }}>{negativeWidth}</div>
-                        <div className="neutral-bar" style={{ width: neutralWidth }}>{neutralWidth}</div>
-                    </div>
-                </div>
-                <div className="list-container md:flex" style={{ width: '100%' }}>
-                <div className={`list-box ${positiveBoxShadowClass}`} style={{ width: '100%', backgroundImage: `url(${article})`, backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative' }}>
-                    <h3 className="list-title">positive</h3>
-                    <p className="article-count">{responseData.article.긍정.length}개</p>
-                    <hr />
-                    <div className="article-list">
-                        {responseData.article.긍정.slice().reverse().map((articleData) => (
-                            <div className="article-item" key={articleData.id}>
-                                <img src={articleData.img || people6} className='article-img' alt="Article" />
-                                <div className="article-content">
-                                    <a href={articleData.link} className="article-link">{articleData.title}</a>
-                                    <p className="write-dt">
-                                    <p className="write-dt">
-                                        {articleData.write_dt.split('T')[0]} {((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[0]}:{((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[1]}</p>
-                                    </p>
-                                </div>
-                                {user_id && (
-                                    <button className="scrap-button" onClick={() => toggleScrap(articleData.id)}>
-                                        <img src={scrapped[articleData.id] ? scrapcomp : scrap} style={{ width: '20px', height: '20px' }} alt="Scrap" />
-                                    </button>
-                                )}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div className={`list-box_ng ${negativeBoxShadowClass}`} style={{ width: '100%', backgroundImage: `url(${article})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    <h3 className="list-title">negative</h3>
-                    <p className="article-count">{responseData.article.부정.length}개</p>
-                    <hr></hr>
-                    <div className="article-list">
-                        {responseData.article.부정.slice().reverse().map((articleData) => (
-                            <div className="article-item" key={articleData.id}>
-                            <img src={articleData.img || people6} className='article-img' alt="Article"/>
-                            <div className="article-content">
-                                <a href={articleData.link} className="article-link">{articleData.title}</a>
-                                <p className="write-dt">{articleData.write_dt.split('T')[0]} {((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[0]}:{((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[1]}</p>
-                            </div>
-                                {user_id && (
-                                    <button className="scrap-button" onClick={() => toggleScrap(articleData.id)}>
-                                        <img src={scrapped[articleData.id] ? scrapcomp : scrap} style={{ width: '20px', height: '20px' }} alt="Scrap" />
-                                    </button>
-                                )}
-                            </div>
-                        ))}
+                    <div className="bar-container mb-2" style={{ width: '100%'}}>
+                        <div className='box'>
+                            <div className="neutral-bar" style={{ width: neutralWidth }}> 중립 {neutralWidth}</div>
+                            <div className="positive-bar" style={{ width: positiveWidth }}> 긍정 {positiveWidth}</div>
+                            <div className="negative-bar" style={{ width: negativeWidth }}> 부정 {negativeWidth}</div>
                         </div>
                     </div>
                 </div>
-                <div className={`list-box ${neutralBoxShadowClass}`} style={{ width: '100%', backgroundImage: `url(${article})`, backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative' }}>
-                    <h3 className="list-title">neutral</h3>
-                    <p className="article-count">{responseData.article.중립.length}개</p>
-                      <hr />
-                        <div className="article-list">
-                          {responseData.article.중립.slice().reverse().map((articleData) => (
-                              <div className="article-item" key={articleData.id}>
-                                  <img src={articleData.img || people6} className='article-img' alt="Article" />
-                                   <div className="article-content">
-                                    <a href={articleData.link} className="article-link">{truncateTitle(articleData.title)}</a>
-                                    <p className="write-dt">
-                                        {articleData.write_dt.split('T')[0]} {((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[0]}:{((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[1]}</p>
-                                         </div>
-                                         {user_id && (
-                                            <button className="scrap-button" onClick={() => toggleScrap(articleData.id)}>
-                                         <img src={scrapped[articleData.id] ? scrapcomp : scrap} style={{ width: '20px', height: '20px' }} alt="Scrap" />
-                                        </button>
-                                    )}
-                            </div>
-                        ))}
+
+
+                <div className='npn-info'>
+
+                    <div className='npn-search'>
+                        <div className='npn'>
+                            <span onClick={handleToggleNeturl} style={{ cursor: 'pointer', fontWeight: showNeturl ? 'bold' : 'normal' }}>Neturl</span> · 
+                            <span onClick={handleTogglePositive} style={{ cursor: 'pointer', fontWeight: showPositive ? 'bold' : 'normal' }}>Positive</span> · 
+                            <span onClick={handleToggleNegative} style={{ cursor: 'pointer', fontWeight: showNegative ? 'bold' : 'normal' }}>Negative</span>
+
+                        </div>
+
+                        <div className="search-related-keywords" style={{width:'50%'}}>
+                            <span>연관 검색어 {responseData.related_keyword.map((related_keyword, index) => (
+                                <span key={index} className="related_keyword" onClick={() => handleKeywordClick(related_keyword)}>
+                                {related_keyword}{index < responseData.related_keyword.length - 1 && ''}
+                                </span>
+                            ))}
+                            </span>
+                        </div>
                     </div>
-                 </div>
+
+                    <hr></hr>
+
+                    <div className='npn-content'>
+                        {showNeturl && (
+                            <div className={`list-box ${neutralBoxShadowClass}`} style={{ width: '100%', backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative' }}>
+                                
+                                <p className="article-count">{responseData.article.중립.length}개</p>
+                                  
+                                    <div className="article-list">
+                                        {responseData.article.중립.slice().reverse().map((articleData) => (
+                                            <div className="article-item" key={articleData.id}>
+                                                <img src={articleData.img || null_img} className='article-img' alt="Article" />
+                                                <div className="article-content">
+                                                <a href={articleData.link} className="article-link">{truncateTitle(articleData.title)}</a>
+                                                <p className="write-dt">
+                                                    {articleData.write_dt.split('T')[0]} {((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[0]}:{((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[1]}</p>
+                                                        </div>
+                                                        {user_id && (
+                                                        <button className="scrap-button" onClick={() => toggleScrap(articleData.id)}>
+                                                        <img src={scrapped[articleData.id] ? scrapcomp : scrap} style={{ width: '20px', height: '20px' }} alt="Scrap" />
+                                                    </button>
+                                                )}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+
+                        {showPositive && (
+                            <div className={`list-box ${positiveBoxShadowClass}`} style={{ width: '100%', backgroundSize: 'cover', backgroundPosition: 'center', position: 'relative' }}>
+                            
+                            <p className="article-count">{responseData.article.긍정.length}개</p>
+                        
+                            <div className="article-list">
+                                {responseData.article.긍정.slice().reverse().map((articleData) => (
+                                    <div className="article-item" key={articleData.id}>
+                                        <img src={articleData.img || null_img} className='article-img' alt="Article" />
+                                        <div className="article-content">
+                                            <a href={articleData.link} className="article-link">{articleData.title}</a>
+                                            <p className="write-dt">
+                                            <p className="write-dt">
+                                                {articleData.write_dt.split('T')[0]} {((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[0]}:{((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[1]}</p>
+                                            </p>
+                                        </div>
+                                        {user_id && (
+                                            <button className="scrap-button" onClick={() => toggleScrap(articleData.id)}>
+                                                <img src={scrapped[articleData.id] ? scrapcomp : scrap} style={{ width: '20px', height: '20px' }} alt="Scrap" />
+                                            </button>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        )}
+                        
+                        {showNegative && (
+                            <div className={`list-box_ng ${negativeBoxShadowClass}`} style={{ width: '100%', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                                
+                                <p className="article-count">{responseData.article.부정.length}개</p>
+
+                                <div className="article-list">
+                                    {responseData.article.부정.slice().reverse().map((articleData) => (
+                                        <div className="article-item" key={articleData.id}>
+                                        <img src={articleData.img || null_img} className='article-img' alt="Article"/>
+                                        <div className="article-content">
+                                            <a href={articleData.link} className="article-link">{articleData.title}</a>
+                                            <p className="write-dt">{articleData.write_dt.split('T')[0]} {((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[0]}:{((articleData.write_dt.split('T')[1]).split('.')[0]).split(':')[1]}</p>
+                                        </div>
+                                            {user_id && (
+                                                <button className="scrap-button" onClick={() => toggleScrap(articleData.id)}>
+                                                    <img src={scrapped[articleData.id] ? scrapcomp : scrap} style={{ width: '20px', height: '20px' }} alt="Scrap" />
+                                                </button>
+                                            )}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+                </div>
+
+
             </div>
-            
         </div>
     );
 }
